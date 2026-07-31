27fefdd42ec25a5416c8e4643434af5ab59963a8
sutitintaintawong-lang <sutitinta.intawong@gmail.com>
2026-07-31 15:56:17 +0700

    Add AIAvatar component for creating digital humans

---
+++ src/pages/AIAvatar.jsx
@@ -0,0 +1,31 @@
+function AIAvatar(){
+
+return(
+
+<div>
+
+<h1>
+🤖 AI Avatar
+</h1>
+
+<p>
+Create talking digital human
+</p>
+
+<textarea
+placeholder="Enter avatar speech..."
+/>
+
+<br/>
+
+<button>
+Generate Avatar
+</button>
+
+</div>
+
+)
+
+}
+
+export default AIAvatar;
