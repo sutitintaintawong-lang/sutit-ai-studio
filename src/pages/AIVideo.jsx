e1f584d7f8851402aea382e0be1c00da896fa9fd
sutitintaintawong-lang <sutitinta.intawong@gmail.com>
2026-07-31 15:55:26 +0700

    Create AIVideo.jsx

---
+++ src/pages/AIVideo.jsx
@@ -0,0 +1,29 @@
+function AIVideo(){
+
+return(
+<div>
+
+<h1>
+🎬 AI Video Generator
+</h1>
+
+<p>
+Transform images into animations
+</p>
+
+<textarea
+placeholder="Describe motion..."
+/>
+
+<br/>
+
+<button>
+Create Video
+</button>
+
+</div>
+)
+
+}
+
+export default AIVideo;
